1afcd207c3847d2138c5d4abbe8099284ff99503
nhanlunars <huynhquocnhan84@gmail.com>
2023-11-27 22:45:03 +0700

    video 72 lesson 61

--- src/services/userService.js
+++ src/services/userService.js
@@ -168,7 +168,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     message: 'Missing required parameter !'
@@ -182,6 +182,11 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+
                 await user.save();
                 resolve({
                     errCode: 0,
