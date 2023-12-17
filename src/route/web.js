302509363d4eb7dd228118146be8c7eebc424bc1
nhanlunars <huynhquocnhan84@gmail.com>
2023-12-17 12:23:27 +0700

    video 85 lesson 74

--- src/route/web.js
+++ src/route/web.js
@@ -29,7 +29,7 @@ let initWebRoutes = (app) => {
     router.get('/api/get-all-doctor', doctorController.getGetAllDoctor);
     router.post('/api/save-infor-doctor', doctorController.postInforDoctor);
     router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
-
+router.post('/api/bulk-create-schedule',doctorController.bulkCreateSchedule);
 
     return app.use("/", router);
 
