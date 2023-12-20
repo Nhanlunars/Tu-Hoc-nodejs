00bc86ab04d215c517c81bc3177b629d95520c17
nhanlunars <huynhquocnhan84@gmail.com>
2023-12-20 18:51:27 +0700

    video 87 lesson 76

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -168,27 +168,15 @@ let bulkCreateSchedule = (data) => {
                     }
                 );
 
-                //convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
-
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 //create data
                 if (toCreate && toCreate.length > 0) {
                     await db.Schedule.bulkCreate(toCreate);
                 }
-
-
-                //console.log('data send: ', schedule)
-                //await db.Schedule.bulkCreate(schedule);
                 resolve({
                     errCode: 0,
                     errMesage: 'OK'
@@ -213,7 +201,12 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] }
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = [];
