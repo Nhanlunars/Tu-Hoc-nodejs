80037f38bb81997fe37662c85ec9cba7f9d672d4
nhanlunars <huynhquocnhan84@gmail.com>
2024-01-17 23:28:12 +0700

    video 101 lesson 90

--- src/models/specialty.js
+++ src/models/specialty.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Specialalty extends Model {
+    class Specialty extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,14 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    Specialalty.init({
+    Specialty.init({
         name: DataTypes.STRING,
-        description: DataTypes.TEXT,
-        image: DataTypes.STRING,
+        descriptionHTML: DataTypes.TEXT,
+        descriptionMarkdown: DataTypes.TEXT,
+        image: DataTypes.TEXT,
     }, {
         sequelize,
-        modelName: 'Specialalty',
+        modelName: 'Specialty',
     });
-    return Specialalty;
+    return Specialty;
 };
