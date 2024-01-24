72a7ad5d13335609bdf409445c3e443b3e12124a
nhanlunars <huynhquocnhan84@gmail.com>
2024-01-24 16:57:10 +0700

    video 103 lesson 92

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -54,18 +54,35 @@ let getGetAllDoctors = () => {
     })
 }
 
+let checkRequiredFields = (inputData) => {
+    let arrFields = ['doctorId', 'contentHTML', 'contentMarkdown', 'action'
+        , 'selectedPrice', 'selectedPayment', 'selectedProvince', 'nameClinic'
+        , 'addressClinic', 'note', 'specialtyId']
+
+    let isValid = true;
+    let element = '';
+    for (let i = 0; i < arrFields.length; i++) {
+        if (!inputData[arrFields[i]]) {
+            isValid = false;
+            element = arrFields[i]
+            break;
+        }
+    }
+    return {
+        isValid: isValid,
+        element: element
+    }
+}
+
 let saveDetailInforDoctors = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action ||
-                !inputData.selectedPrice || !inputData.selectedPayment ||
-                !inputData.selectedProvince || !inputData.nameClinic ||
-                !inputData.addressClinic || !inputData.note
-            ) {
+            let checkOnj = checkRequiredFields(inputData);
+
+            if (checkOnj.isValid === false) {
                 resolve({
                     errCode: 1,
-                    errMesage: 'Missing parameter'
+                    errMessage: `Missing parameter: ${checkOnj.element}`
                 })
             } else {
                 //upsert to Markdown table
@@ -108,6 +125,8 @@ let saveDetailInforDoctors = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.note;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
                     await doctorInfor.save()
 
                 } else {
@@ -120,13 +139,15 @@ let saveDetailInforDoctors = (inputData) => {
                         nameClinic: inputData.nameClinic,
                         addressClinic: inputData.addressClinic,
                         note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId
                     })
 
                 }
 
                 resolve({
                     errCode: 0,
-                    errMesage: 'Save infor doctor succeed'
+                    errMessage: 'Save infor doctor succeed'
                 })
             }
 
