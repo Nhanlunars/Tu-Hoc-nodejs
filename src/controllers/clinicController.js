a1cd6e6d100e251f75f706dd12cd3c63f5858b58
nhanlunars <huynhquocnhan84@gmail.com>
2024-02-29 11:02:38 +0700

    video 110 lesson 97

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -15,6 +15,39 @@ let createClinic = async (req, res) => {
     }
 }
 
+let getAllClinic = async (req, res) => {
+    try {
+        let infor = await clinicServices.getAllClinic();
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let infor = await clinicServices.getDetailClinicById(req.query.id);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
     createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById,
+
 }
